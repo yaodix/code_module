7cf9ab70881ee5f48a2c8e5cf656f463408ba930
yaodix <liuyao199111@163.com>
2023-06-11 22:24:32 +0800

    add kf demo

---
+++ kalman_filter/kalman_filter.h
@@ -0,0 +1,37 @@
+/*
+ 参考： https://blog.csdn.net/shuoyueqishilove/article/details/81713142
+
+
+*/
+#ifndef _MYKALMAN_H
+#define _MYKALMAN_H
+
+#include <eigen3/Eigen/Dense>
+
+
+class KalmanFilter
+{
+private:
+    int stateSize; // state variable's dimenssion
+    int measSize;  // measurement variable's dimession
+    int uSize;     // control variables's dimenssion
+    Eigen::VectorXd x;
+    Eigen::VectorXd z;  // k时刻测量值
+    Eigen::MatrixXd A;  // 系统参数
+    Eigen::MatrixXd B;  // 系统参数
+    Eigen::VectorXd u;
+    Eigen::MatrixXd P;  // coveriance
+    Eigen::MatrixXd H;  // 测量系统的参数
+    Eigen::MatrixXd R;  // measurement noise covariance
+    Eigen::MatrixXd Q;  // process noise covariance
+
+public:
+    KalmanFilter(int stateSize_, int measSize_,int uSize_);
+    ~KalmanFilter(){}
+    void init(Eigen::VectorXd &x_, Eigen::MatrixXd& P_,Eigen::MatrixXd& R_, Eigen::MatrixXd& Q_);
+    Eigen::VectorXd predict(Eigen::MatrixXd& A_);
+    Eigen::VectorXd predict(Eigen::MatrixXd& A_, Eigen::MatrixXd &B_, Eigen::VectorXd &u_);
+    void update(Eigen::MatrixXd& H_,Eigen::VectorXd z_meas);
+};
+
+#endif
