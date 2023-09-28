350835102ccadc3f82afcbfe01adc56d6c8c79c0
liuyao <liuyao@zhito.com>
2023-09-28 18:05:39 +0800

    add graph demo

--- game-programming-patterns/graph/test.cc
+++ game-programming-patterns/graph/test.cc
@@ -1,8 +1,16 @@
-#include "./cxxgraph/include/CXXGraph.hpp"
+#include "cxxgraph/include/CXXGraph.hpp"
 
 int main() {
-  CXXGraph::Graph<int> gra;
-  gra.addEdge()
+  CXXGraph::Node<int> node1("1", 1);
+  CXXGraph::Node<int> node2("2", 2);
+  std::pair<const CXXGraph::Node<int> *, const CXXGraph::Node<int> *> pairNode(
+      &node1, &node2);
+  CXXGraph::Edge<int> edge(1, pairNode);
+  CXXGraph::T_EdgeSet<int> edgeSet;
+  edgeSet.insert(make_shared<CXXGraph::Edge<int>>(edge));
+  CXXGraph::Graph<int> graph(edgeSet);
+
+  graph.writeToFile("./g.txt");
   
   return 0;
 }
